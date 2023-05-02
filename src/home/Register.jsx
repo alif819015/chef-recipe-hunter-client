c311d203e455352243cb560429eb797acde709de
Mahmud Hasan <mahmudhassan11752@gmail.com>
2023-05-02 12:29:25 +0600

    3rd commit added loaddin and register page

--- src/home/Register.jsx
+++ src/home/Register.jsx
@@ -1,11 +1,126 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useContext, useState } from "react";
+import { Form, Link } from "react-router-dom";
+import { AuthContext } from "../auth/AuthProvider";
 
 const Register = () => {
-    return (
-        <div>
-            
+  const { createUser } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, photo, email, password);
+
+    setSuccess('');
+    setError('');
+
+    if(!/(?=.*[A-Z])/.test(password)) {      
+        setError('Please at least add one Uppercase');
+        return;
+    }
+    else if(!/(?=.*[!@#$&*])/.test(password)){
+        setError('Please add a special Character')
+        return;
+    }
+    else if(password.length < 6){
+        setError('Please at least 6 character in your password')
+        return;
+    }
+    createUser(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        setSuccess("User has create successfully");
+        event.target.reset();
+      })
+      .catch((error) => {
+        console.error(error.message);
+        setError(error.message);
+      });
+  };
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col ">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Please Login!</h1>
         </div>
-    );
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <Form onSubmit={handleRegister} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type Your name"
+                name="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                placeholder="url"
+                name="photo"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                name="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Register</button>
+            </div>
+            <p className="text-red-600">
+              <small>{error}</small>
+            </p>
+            <p className="text-green-600">
+              <small>{success}</small>
+            </p>
+            <a className="label-text-alt">
+              Already have an Account{" "}
+              <span className="text-primary font-semibold link link-hover">
+                {" "}
+                <Link to="/login">Login</Link>
+              </span>
+            </a>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
